64561c3eeefdc0e2598f4ce06fcb7424539491d7
mdaurbekov <dreamkov@gmail.com>
2023-01-09 00:10:42 +0300

    task_6876

---
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -0,0 +1,28 @@
+package ru.job4j.di;
+
+public class StartUI {
+
+    private Store store;
+
+    private ConsoleInput consoleInput;
+
+    public StartUI(Store store, ConsoleInput consoleInput) {
+        this.store = store;
+        this.consoleInput = consoleInput;
+    }
+
+    public void add(String value) {
+        store.add(value);
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+
+    public void ask(String question) {
+        System.out.println(consoleInput.askStr(question));
+    }
+
+}
---
+++ src/main/java/ru/job4j/di/model/Item.java
@@ -0,0 +1,9 @@
+package ru.job4j.di.model;
+
+public class Item {
+
+    private int id;
+    private String name;
+
+
+}
