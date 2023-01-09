aa7ea920558bdd75054e5a9b9a09ebc118382600
mdaurbekov <dreamkov@gmail.com>
2023-01-09 20:22:03 +0300

    task_2091

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -2,19 +2,15 @@ package ru.job4j.di;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Main {
+public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(Store.class);
-        context.register(ConsoleInput.class);
         context.register(StartUI.class);
         context.refresh();
-
         StartUI ui = context.getBean(StartUI.class);
-
         ui.add("Petr Arsentev");
         ui.add("Ivan ivanov");
         ui.print();
-        ui.ask("Вопрос");
     }
 }
